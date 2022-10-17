f9aabc67b9fadaaa795f2d65071d5bd2e1560504
rogemaster <rlaaudwn000@naver.com>
2022-10-17 23:27:14 +0900

    react-nextjs-typescript app

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,50 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import React from 'react'
+import { getAllPostIds, getPostData } from '../../lib/post';
+import postStyles from '../../styles/Post.module.css';
+
+const Post = ({ postData }: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  }
+}) => {
+  return (
+    <div className={postStyles.container}>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1>{postData.title}</h1>
+        <div>
+          {postData.date}
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+      
+    </div>
+  )
+}
+
+export default Post;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  console.log('paths:: ', paths);
+  //  [{params: {id: 'pre-rendering'}}]
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string);
+  return {
+    props: {
+      postData,
+    }
+  }
+}
